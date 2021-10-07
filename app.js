cc93f3bbc61ccbbb2b079c0d9299486936855740
Marcos Scheuermann <scheuermannjm@gmail.com>
2021-10-07 19:20:17 -0300

    clase javascript

---
+++ app.js
@@ -0,0 +1,71 @@
+//console.log("hola mundo")
+//alert("HOLIS")
+//! soy un comentario
+//*soy un comercario de una sola 
+
+//variable
+
+let variable1 = 1;
+var variable2 = 2; //!NO ES UNA BUENA PRACTICA
+const variable3 = 3;
+
+console.log(variable2);
+
+//tipos de variables
+//string
+
+let string = "soy un string";
+
+// number
+
+let numero = 15;
+
+//boolean
+
+let boolean = false;
+let booleanv = true;
+
+//array
+
+let arreglo = [1, 2, 3, 4];
+let arreglo2 = [1, "nada", true]
+
+//objet
+let sanmartin = {
+    nombre:"san martin", 
+    lugar: "tucuman",
+    edad: 15}
+
+//null
+let nada = null;
+
+//indefinida
+
+let indefinida;
+
+//operadores
+
+//ejercicio dos valores y muestro la suma y el producto, si la suma es mayor a 20 mostrar mostrar que hay un valor mayor a 20.
+
+let a = prompt ("soy una variable a");
+let b = prompt ("soy otra variable b");
+let suma = a+b;
+let prod = a*b;
+
+console.log (suma);
+console.log (prod);
+
+
+
+
+
+let valor1 = 11;
+let valor2 = 9;
+prompt (valor1+valor2);
+console.log(valor1+valor2);
+
+
+
+
+
+
